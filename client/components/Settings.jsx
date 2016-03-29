e3d3f3aab4583e41be485c4ccf6663b5dc79f5ec
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2016-03-29 10:46:55 +0200

    added list of user on settings view

--- client/components/Settings.jsx
+++ client/components/Settings.jsx
@@ -1,12 +1,61 @@
 Settings = React.createClass({
 
+  getInitialState: function() {
+    return {
+      users: []
+    };
+   },
+
+  componentDidMount() {
+    const that = this;
+    Meteor.call("room.users", this.props.roomId , function(error, result){
+      that.setState({users: result});
+    });
+    $('.ui.accordion')
+      .accordion()
+    ;
+  },
+
 
   render() {
+    const users = [];
+    this.state.users.forEach(function(user) {
+      users.push(
+        <div className="item">
+          <img className="ui avatar image" src="http://localhost:3000/packages/jorgeer_chatter-semantic/public/images/avatar.jpg"/>
+          <div className="content">
+            <a className="header">
+              {user.nick}
+            </a>
+            <div className="description">
+              Last logged in just now.
+            </div>
+          </div>
+        </div>
+      );
+    });
 
     return (
-      <div className="">
-        settings dude
+      <div className="padded">
+        <div className="ui header">
+          Settings
+        </div>
+        <div className="ui accordion">
+          <div className="title">
+            <i className="dropdown icon"></i>
+            Users
+          </div>
+          <div className="content">
+            <div className="ui list relaxed">
+              {users}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 });
+
+
+
+
