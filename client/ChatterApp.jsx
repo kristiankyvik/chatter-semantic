aefa074d702e25bd23fdc83db3e1723ca4ec5e7c
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2016-12-19 10:40:49 +0100

    set the session specifying the initial message load to use the one defined in the config

--- client/ChatterApp.jsx
+++ client/ChatterApp.jsx
@@ -25,7 +25,7 @@ const ChatterApp = React.createClass({
 
   getInitialState: function () {
     Session.set("chatOpen", false);
-    Session.setDefault('messageLimit', 100);
+    Session.setDefault('messageLimit', Chatter.options.messageLimit);
 
     return {
       chatOpen: false,
